549c84e00e1088212eaecb3706eb0dc5c049d2f5
Benjamin Barnett <benjaminbarnett@Benjamins-MacBook-Air.local>
2023-11-25 20:04:40 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -5,6 +5,45 @@ const cookieParser = require('cookie-parser'); // Express web server framework
 
 var app = express();
 
+var nodemailer = require('nodemailer');
+
+var maillist = [
+  'thejkbxapp@gmail.com',
+  'josephedaquila@gmail.com'
+];
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'aplusneveraminus@gmail.com',
+    pass: 'zuguingmtigigugz'
+  }
+});
+
+app.get('/contact', function(req, res) {
+  var q = req.query;
+  var first, email, phone, message;
+  first = q.name;
+  email = q.email;
+  phone = q.phone;
+  message = q.message;
+  mailOptions = {
+    from: 'aplusneveraminus@gmail.com',
+    to: maillist,
+    subject: 'New contact from website...',
+    text: first + '.\n' + email + '.\n' + phone + '.\n' + message
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.send("ERROR");
+    } else {
+      res.send("SUCCESS");
+    }
+  });
+
+});
+
 
 app.use(express.static(__dirname + '/www'))
     .use(cookieParser());
