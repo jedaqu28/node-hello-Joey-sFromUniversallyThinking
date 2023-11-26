59534e8515c679fbc56a12afdf206c7c95667dcf
Benjamin Barnett <benjaminbarnett@Benjamins-MacBook-Air.local>
2023-11-25 19:48:27 -0500

    TEST

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const express = require('express'); // Express web server framework
+const cookieParser = require('cookie-parser'); // Express web server framework
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+var app = express();
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+
+app.use(express.static(__dirname + '/www'))
+    .use(cookieParser());
+
+
+app.listen(8080);
+
+console.log("Visit http://localhost:8080 in your browser!");
